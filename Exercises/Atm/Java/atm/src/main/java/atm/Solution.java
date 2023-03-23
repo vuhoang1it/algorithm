314ea5b6cf7bc5dd13b20ec015646758bda4c6a3
vuhoang <vu.hoang@ycloudassethealth.onmicrosoft.com>
2023-03-23 15:28:21 +0700

    update java project

---
+++ Exercises/Atm/Java/atm/src/main/java/atm/Solution.java
@@ -0,0 +1,16 @@
+package atm;
+
+public class Solution {
+	
+	public int[] atmSolution(int n)
+	{
+		//TODO
+		throw new UnsupportedOperationException();
+	}
+	
+	public int[] atmSolutionWithLimit(int n)
+	{
+		//TODO
+		throw new UnsupportedOperationException();
+	}
+}
